70abaa299fb75bb4c6544e5614fbc7b85681f790
Ria <157780826+ArianneCielo@users.noreply.github.com>
2025-05-08 11:09:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,12 @@ window.MathJax = {
         inlineMath: [['$', '$'], ['\\(', '\\)']]
     }
 };
+
+
+const menuButton = document.getElementById("menu-button");
+        const sidebar = document.getElementById("sidebar");
+
+        menuButton.addEventListener("click", () => {
+            sidebar.style.display = sidebar.style.display === "block" ? "none" : "block";
+            
+        });
